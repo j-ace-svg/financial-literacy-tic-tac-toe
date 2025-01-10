432ea8b439877f2e6576417aadd02d3ea355da8f
John Montgomery <j.ace.svg@gmail.com>
2025-01-10 12:57:23 -0600

    Create display loop

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
+import pygame
+from pygame.locals import *
+import random
+import os
 
+pygame.init()
+
+fps = 60
+
+screen_width = 864
+screen_height = 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Financial Literacy Tic-Tac-Toe')
+
+while True:
+    clock.tick(fps)
+
+    pygame.display.update()
