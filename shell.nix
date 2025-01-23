42f7a0cf72ea895a74fc12ded0b529ac2a6f0ab0
John Montgomery <j.ace.svg@gmail.com>
2025-01-22 23:13:49 -0600

    Add flake-based development environment

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable") {} }:
-
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    (python3.withPackages (ps: [ ps.pygame-ce ]))
-  ];
-}
